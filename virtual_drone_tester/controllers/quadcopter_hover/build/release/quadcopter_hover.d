8476abd3f09fbdac253a588c40bb251f1d858245
GraceYuRPI <138412596+GraceYuRPI@users.noreply.github.com>
2024-03-19 17:07:04 -0400

    Add files via upload

---
+++ virtual_drone_tester/controllers/quadcopter_hover/build/release/quadcopter_hover.d
@@ -0,0 +1,10 @@
+build/release/quadcopter_hover.o: quadcopter_hover.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/InertialUnit.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Gyro.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/GPS.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Keyboard.hpp
