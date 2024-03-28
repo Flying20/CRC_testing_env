7de75ecbcd73c3ba68748987f80c0f0128cf89ef
Flying20 <davidchen0024@gmail.com>
2024-03-28 16:21:12 -0400

    Test commit with initial draft of quadcopter attitide control

---
+++ virtual_drone_tester/controllers/quadcopter_attitude/build/release/quadcopter_attitude.d
@@ -0,0 +1,11 @@
+build/release/quadcopter_attitude.o: quadcopter_attitude.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/InertialUnit.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Gyro.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/GPS.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Keyboard.hpp \
+ pid.hpp
